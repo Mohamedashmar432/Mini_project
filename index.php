05f947ed3aa0200a87c019b9952f9d4743656528
Mohamedashmar432 <mohamedashmar432@gmail.com>
2024-03-23 00:33:50 +0530

    css file for all module has been added

--- index.php
+++ index.php
@@ -6,181 +6,11 @@ include "libs/load.php";
 
 <head>
   <script src="js/color-modes.js"></script>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-
   <link href="css/bootstrap.min.css" rel="stylesheet">
-
-  <style>
-    .bd-placeholder-img {
-      font-size: 1.125rem;
-      text-anchor: middle;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      user-select: none;
-    }
-
-    @media (min-width: 768px) {
-      .bd-placeholder-img-lg {
-        font-size: 3.5rem;
-      }
-    }
-
-    .b-example-divider {
-      width: 100%;
-      height: 3rem;
-      background-color: rgba(0, 0, 0, .1);
-      border: solid rgba(0, 0, 0, .15);
-      border-width: 1px 0;
-      box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-    }
-
-    .b-example-vr {
-      flex-shrink: 0;
-      width: 1.5rem;
-      height: 100vh;
-    }
-
-    .bi {
-      vertical-align: -.125em;
-      fill: currentColor;
-    }
-
-    .nav-scroller {
-      position: relative;
-      z-index: 2;
-      height: 2.75rem;
-      overflow-y: hidden;
-    }
-
-    .nav-scroller .nav {
-      display: flex;
-      flex-wrap: nowrap;
-      padding-bottom: 1rem;
-      margin-top: -1px;
-      overflow-x: auto;
-      text-align: center;
-      white-space: nowrap;
-      -webkit-overflow-scrolling: touch;
-    }
-
-    .btn-bd-primary {
-      --bd-violet-bg: #712cf9;
-      --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-      --bs-btn-font-weight: 600;
-      --bs-btn-color: var(--bs-white);
-      --bs-btn-bg: var(--bd-violet-bg);
-      --bs-btn-border-color: var(--bd-violet-bg);
-      --bs-btn-hover-color: var(--bs-white);
-      --bs-btn-hover-bg: #6528e0;
-      --bs-btn-hover-border-color: #6528e0;
-      --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-      --bs-btn-active-color: var(--bs-btn-hover-color);
-      --bs-btn-active-bg: #5a23c8;
-      --bs-btn-active-border-color: #5a23c8;
-    }
-
-    .bd-mode-toggle {
-      z-index: 1500;
-    }
-
-    .bd-mode-toggle .dropdown-menu .active .bi {
-      display: block !important;
-    }
-
-    .col {
-      position: relative;
-      top: 20px;
-
-    }
-
-    .nav-link1 {
-      position: absolute;
-      left: 10px;
-      display: block;
-      padding: var(--bs-nav-link-padding-y) var(--bs-nav-link-padding-x);
-      font-size: var(--bs-nav-link-font-size);
-      font-weight: var(--bs-nav-link-font-weight);
-      color: var(--bs-nav-link-color);
-      text-decoration: none;
-      background: 0 0;
-      border: 0;
-      transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out;
-    }
-
-    .mar {
-      margin: 20px;
-    }
-
-    .pre1 {
-      margin-top: 200px;
-      margin-bottom: 200px;
-    }
-
-    .row {
-      position: relative;
-      top: 10px;
-
-    }
-
-    .card {
-      --bs-card-border-color: var(--bs-info-border-subtle);
-
-    }
-
-    #businessbut {
-      position: relative;
-      top: -180px;
-      left: 800px;
-      /* --bs-btn-border-color: var(--bs-info-border-subtle); */
-      /* --bs-btn-hover-color: #fff; */
-      /* --bs-btn-hover-bg: var(--bs-info-border-subtle); */
-      /* --bs-btn-hover-border-color: var(--bs-info-border-subtle); */
-      /* --bs-btn-color:white; */
-    }
-
-    .carousel-caption {
-      position: relative;
-    }
-
-    a {
-      color: rgb(221 227 235);
-      text-decoration: unset;
-    }
-
-    #particle-js {
-      width: 100%;
-      height: 100%;
-      background-color: transparent;
-    }
-
-    .container {
-      margin: 20px;
-    }
-
-    .fs-5 {
-      text-align: justify;
-    }
-
-    .nav-item.dropdown {
-
-      left: 875px !important;
-      margin-top: var(--bs-dropdown-spacer);
-    }
-
-    .about {
-      position: relative;
-      top: 25px;
-    }
-  </style>
-
-
-  <!-- Custom styles for this template -->
+  <link href="css/index.css" rel="stylesheet">
   <link href="css/carousel.css" rel="stylesheet">
 </head>
 
@@ -248,23 +78,11 @@ include "libs/load.php";
 
   <?php load('header'); ?>
   <main>
-
     <?php load('slider'); ?>
-
-
-
-    <!-- Marketing messaging and featurettes
-  ================================================== -->
-    <!-- Wrap the rest of the page in another container to center all the content. -->
-
     <?php load('marketer'); ?>
-
-    <!-- FOOTER -->
     <?php load('footer'); ?>
   </main>
   <script src="js/bootstrap.bundle.min.js"></script>
-
-
 </body>
 
 </html>
--- test.php
+++
@@ -1,8 +0,0 @@
-<pre>
-<?php
-include 'libs/user.class.php';
-print_r($_POST);
-print_r($_FILES);
-
-// user::sell("mak1e","pr1odect","du1bai","exa1m@gamil.com","image");
-?>
